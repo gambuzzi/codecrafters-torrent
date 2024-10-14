0d8e5070159c026de07915f015099a1ed7906d3c
Roberto Gambuzzi <gambuzzi@gmail.com>
2024-10-14 17:34:43 +0100

    fc

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@ use serde_json;
 use std::env;
 
 // Available if you need it!
-// use serde_bencode
+use serde_bencode;
+use serde_bencode::value::Value;
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+fn map_bencode_to_json(value: Value) -> serde_json::Value {
+    match value {
+        Value::Bytes(v) => serde_json::Value::String(String::from_utf8_lossy(&v).to_string()),
+        Value::Int(v) => {
+            v.into()
+            // serde_json::Value::Number(serde_json::value::Number::from_f64(v as f64).unwrap())
+        }
+        Value::List(v) => {
+            serde_json::Value::Array(v.into_iter().map(map_bencode_to_json).collect())
+        }
+        Value::Dict(v) => {
+            let map: serde_json::Map<String, serde_json::Value> = v
+                .into_iter()
+                .map(|(k, v)| {
+                    let string_k: String = String::from_utf8_lossy(&k).to_string();
+                    let val = map_bencode_to_json(v);
+                    (string_k, val)
+                })
+                .collect();
+            serde_json::Value::Object(map)
+        }
     }
 }
 
@@ -25,13 +35,9 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
